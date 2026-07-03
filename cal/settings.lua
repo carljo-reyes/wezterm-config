88cfc6e75be035e660d7804a583196846340f078
Carljo Rommel Reyes <zcarljorommel.reyes@gcash.com>
2026-07-03 13:24:36 +0800

    set default window size

--- cal/settings.lua
+++ cal/settings.lua
@@ -1,7 +1,11 @@
 local apply = function(config)
     config.automatically_reload_config = true
+    config.font_size = 14
+    config.initial_cols = 130
+    config.initial_rows = 40
     config.window_background_opacity = 0.98
-    config.window_decorations = "RESIZE"
+
+    -- config.window_decorations = "RESIZE"
 end
 
 return {
