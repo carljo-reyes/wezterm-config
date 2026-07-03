ccc65bcc9d61d5fbe6ed1ba0cd1a60ef3cd904a0
Carljo Rommel Reyes <zcarljorommel.reyes@gcash.com>
2026-07-03 13:24:02 +0800

    set switching panes HJKL

--- cal/keys.lua
+++ cal/keys.lua
@@ -7,10 +7,49 @@ local wezterm = require 'wezterm'
 
 return {
     {
+        key = 'H',
+        mods = 'CMD|SHIFT',
+        action = wezterm.action.ActivatePaneDirection 'Left'
+    },
+    {
+        key = 'J',
+        mods = 'CMD|SHIFT',
+        action = wezterm.action.ActivatePaneDirection 'Down'
+    },
+    {
+        key = 'K',
+        mods = 'CMD|SHIFT',
+        action = wezterm.action.ActivatePaneDirection 'Up'
+    },
+    {
+        key = 'L',
+        mods = 'CMD|SHIFT',
+        action = wezterm.action.ActivatePaneDirection 'Right'
+    },
+    {
+        key = 'Z',
+        mods = 'CMD|SHIFT',
+        action = wezterm.action.TogglePaneZoomState
+    },
+    {
+        key = '9',
+        mods = 'ALT',
+        action = wezterm.action.ShowLauncherArgs {
+            flags = 'FUZZY|WORKSPACES',
+        },
+    },
+    {
         key = '"',
         mods = 'CMD|SHIFT',
         action = wezterm.action.SplitPane {
             direction = 'Down',
+        },
+    },
+    {
+        key = "'",
+        mods = 'CMD',
+        action = wezterm.action.SplitPane {
+            direction = 'Right',
         },
     },
 }
