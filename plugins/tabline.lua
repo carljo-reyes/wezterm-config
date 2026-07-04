fb2591e5128fe2d903e67423ce13968181337df7
Carljo Rommel Reyes <zcarljorommel.reyes@gcash.com>
2026-07-04 13:55:59 +0800

    fix-tabline-nitpicks

--- plugins/tabline.lua
+++ plugins/tabline.lua
@@ -7,32 +7,36 @@ end
 local apply = function(config)
     local theme = config.color_scheme
     local colors = wezterm.color.get_builtin_schemes()[theme]
-    local surface = colors.cursor and colors.cursor.bg or colors.ansi[1]
-    local background = colors.tab_bar and colors.tab_bar.inactive_tab and colors.tab_bar.inactive_tab.bg_color or colors.background
+    -- Tab bar uses black; terminal background stays purple from the colorscheme.
+    local surface = colors.ansi[1]
 
     tabline.setup({
         options = {
             icons_enabled = true,
-            -- theme = theme,
+            theme = theme,
             tabs_enabled = true,
             theme_overrides = {
+                normal_mode = {
+                    -- c.bg is used as separator/tab-bar fill behind powerline glyphs
+                    c = { fg = colors.foreground, bg = surface },
+                },
                 tab = {
                     inactive = { fg = colors.ansi[5], bg = surface },
-                    active = { fg = colors.ansi[6], bg = background },
+                    active = { fg = colors.ansi[6], bg = surface },
                     inactive_hover = { fg = colors.ansi[6], bg = surface },
-                }
+                },
             },
             section_separators = {
-                left = wezterm.nerdfonts.ple_lower_left_triangle,
-                right = wezterm.nerdfonts.ple_upper_right_triangle,
+                left = wezterm.nerdfonts.pl_left_hard_divider,
+                right = wezterm.nerdfonts.pl_right_hard_divider,
             },
             component_separators = {
-                left = wezterm.nerdfonts.ple_lower_left_triangle,
-                right = wezterm.nerdfonts.ple_upper_right_triangle,
+                left = wezterm.nerdfonts.pl_left_soft_divider,
+                right = wezterm.nerdfonts.pl_right_soft_divider,
             },
             tab_separators = {
-                left = wezterm.nerdfonts.ple_lower_left_triangle,
-                right = wezterm.nerdfonts.ple_upper_right_triangle,
+                left = wezterm.nerdfonts.pl_left_hard_divider,
+                right = wezterm.nerdfonts.pl_right_hard_divider,
             },
         },
         sections = {
@@ -63,6 +67,11 @@ local apply = function(config)
         },
         extensions = {},
     })
+
+    tabline.apply_to_config(config)
+    -- config.colors = config.colors or {}
+    -- config.colors.tab_bar = config.colors.tab_bar or {}
+    -- config.colors.tab_bar.background = surface
 end
 
 return {
